678e2855818ff8821568f6430d34bb8f79ac8f63
Arman Jakupovic <6430717+ArmanJakupovic@users.noreply.github.com>
2024-03-16 19:49:01 +0100

    Change script to have arguments
    New arguments are:
    -m for model id
    -w for max workers.

--- huggingface/scripts/hf_download.py
+++ huggingface/scripts/hf_download.py
@@ -1,16 +1,24 @@
-import sys
+import argparse
 from huggingface_hub import snapshot_download
 
-def download_model(model_id):
+def download_model(model_id, max_workers):
     # Split the model_id on '/' and take the second part as the model_name
     model_name = model_id.split('/')[1]
     snapshot_download(repo_id=model_id, local_dir=model_name,
-                      local_dir_use_symlinks=False, revision="main", max_workers=2)
+                      local_dir_use_symlinks=False, revision="main", max_workers=max_workers)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py model_id")
-        sys.exit(1)
+    # Create the parser
+    parser = argparse.ArgumentParser(description='Download a model from the Hugging Face Hub')
 
-    model_id = sys.argv[1]
-    download_model(model_id)
+    # Add an argument for the model ID
+    parser.add_argument('-m', '--model_id', help='The HuggingFace Hub model ID to download. Format should be <user>/<model_name>.', required=True)
+
+    # Add an argument for max_workers with '-w' as the option
+    parser.add_argument('-w', '--max_workers', type=int, default=2, help='The maximum number of workers to use for downloading. Default is 2.')
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # Use the model_id and max_workers arguments
+    download_model(args.model_id, args.max_workers)
